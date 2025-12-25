4588848e2ed9bf62908d248b4ac885eb6e9f686a
NguyenThaiDuong2308 <03thaiduong@gmail.com>
2025-12-25 17:46:57 +0700

    change server

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://thanhserver123.me:444";
-export const CHAT_API_URL = "https://thanhserver123.me:444";
-export const WS_BASE_URL = "wss://thanhserver123.me:444/ws/chat";
+export const API_BASE_URL = "https://livehub.website:444";
+export const CHAT_API_URL = "https://livehub.website:444";
+export const WS_BASE_URL = "wss://livehub.website:444/ws/chat";
